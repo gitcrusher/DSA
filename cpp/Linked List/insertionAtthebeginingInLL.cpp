c28fd6055c861d150078e11e8e9b6e2550f700a4
gitcrusher <aayushguptasoni@gmail.com>
2024-09-01 17:57:53 +0530

    commit

---
+++ cpp/Linked List/insertionAtthebeginingInLL.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+    class node
+    {
+
+    public:
+    int data;
+    node* next;
+    node(int x){
+        data = x;
+        next = nullptr;
+    }
+    };
+
+    node* insertbegin(node*head,int x ){
+        node* temp = new node(x);
+        temp->next = head;
+        return temp;
+    }
+    
+    void printList(node* head) {
+    node* current = head;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+int main (){
+    // int n ;
+    // cout<<"n = ";
+    // cin>>n;
+    node *head =   NULL;
+    head= insertbegin(head, 30);
+    head = insertbegin(head, 20);
+    head = insertbegin(head, 10);
+    printList(head);
+return 0;
+}
