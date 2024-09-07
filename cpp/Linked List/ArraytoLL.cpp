cb6ff52b2a8860c441c2cc9292d93d684b84ba8a
gitcrusher <aayushguptasoni@gmail.com>
2024-09-08 02:05:38 +0530

    commit

--- cpp/Linked List/ArraytoLL.cpp
+++ cpp/Linked List/ArraytoLL.cpp
@@ -5,22 +5,24 @@ using namespace std;
         public:
         int data ;
         Node* next;
-        Node(int data1, Node*next1){
+        Node(int data1){
             data = data1;
-            next = next1;
+            next = nullptr;
         }
     };
-
-int main (){
-    int n;
-    cout<<"No of  element: ";
-    cin>>n;
-    int arr[n];
-    for(int i = 0 ; i < n ; i ++){
-        cin>>arr[i];
+Node* convert(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 0 ; i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover -> next = temp ;
+        mover = temp;
     }
-    Node*y = new Node(arr[0], nullptr);
-    cout<<y->data;
-
+    return head;
+}
+int main (){
+    vector<int> arr = {2,3,4,5};
+    Node* head = convert(arr);
+    cout<<head->data;
 return 0;
 }
