a7f0e47ffdb27ca2558284f64f59c8ca16c33ce2
gitcrusher <aayushguptasoni@gmail.com>
2024-09-21 10:16:55 +0530

    commit

--- cpp/Arrays/RemoveDuplicateElements.cpp
+++ cpp/Arrays/RemoveDuplicateElements.cpp
@@ -1,20 +1,27 @@
-#include<bits/stdc++.h>
-using namespace std;
-    
-int main (){
-    int n ;
-    cout<<"Enter the size of array: ";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter the elements of array: ";
-    for(int i = 0 ; i<n;i++){
-        cin>>arr[i];
-    }
+    #include<bits/stdc++.h>
+    using namespace std;
+        
+    int main (){
+        int n ;
+        cout<<"Enter the size of array: ";
+        cin>>n;
+        int arr[n];
+        cout<<"Enter the elements of array: ";
+        for(int i = 0 ; i<n;i++){
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        int x = 0;
+        for(int i = 1 ; i < n ; i++){
+            if(arr[i]!=arr[x]){
+                x=x+1;
+                arr[x]=arr[i];
 
-    sort(arr,arr+n);
-    
-    for(int i = 0   ; i < n ; i++){
-        cout<<arr[i]<<' ';
+            }
+        }
+
+        for(int i = 0   ; i <= x ; i++){
+            cout<<arr[i]<<' ';
+        }
+    return 0;
     }
-return 0;
-}
