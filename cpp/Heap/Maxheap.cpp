e4009e719fc8af089dc6f67f6fc3755926aa8fa9
gitcrusher <aayushguptasoni@gmail.com>
2024-11-20 11:45:07 +0530

    commit

--- cpp/Heap/Maxheap.cpp
+++ cpp/Heap/Maxheap.cpp
@@ -3,14 +3,14 @@ using namespace std;
     void Maxheapify(int a[],int n , int i){
         int largest = i;
         int left = 2*i;
-        int right = 2*i*+1;
+        int right = 2*i+1;
         if(left<n&&a[left]>a[largest]){
             largest = left;
         }
         if(right<n&&a[right]>a[largest]){
             largest = right;
         }
-        if(!largest==i){
+        if(largest!=i){
             swap(a[i],a[largest]);
             Maxheapify(a,n,largest);
         }
