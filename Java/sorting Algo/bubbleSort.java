2cc73ed2a8fddb46a22f0d1e4d93b7ca426fd0a9
gitcrusher <aayushguptasoni@gmail.com>
2025-02-05 12:47:00 +0530

    commit

---
+++ Java/sorting Algo/bubbleSort.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+
+public class bubbleSort {
+    public static void BubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean value;
+        for (int i = 0; i < n - 1; i++) {
+            value = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    value = true;
+                }
+            }
+            if (!value) break;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Enter the next number : ");
+        int[] arr = new int[n];
+        System.out.println("Enter the array elements : ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Original array: " +arr);
+        
+        BubbleSort(arr);
+        
+        System.out.println("Sorted array: " + arr);
+        sc.close();
+    }
+}
